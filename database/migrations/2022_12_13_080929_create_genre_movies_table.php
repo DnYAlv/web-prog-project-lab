4fe4e7e7acb18739f7d94a4e3e6bac3f24b10022
DnYAlv <dennyginting40@gmail.com>
2022-12-13 15:42:58 +0700

    Migration Complete

--- database/migrations/2022_12_13_080929_create_genre_movies_table.php
+++ database/migrations/2022_12_13_080929_create_genre_movies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateGenreMoviesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('genre_movies', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('checkAdmin')->default(false);
-            $table->rememberToken();
+            $table->foreignId('genre_id')->constrained('genres');
+            $table->foreignId('movie_id')->constrained('movies');
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('genre_movies');
     }
 }
