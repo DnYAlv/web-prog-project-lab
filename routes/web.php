b249865b1faaddeba04ec7539903e41b0dce1c81
DnYAlv <dennyginting40@gmail.com>
2022-12-27 10:51:52 +0700

    Routing and middleware logic for login and movie is more likely to be completed, TESTING needed, and since testing needed, FRFE design needed to validate

--- routes/web.php
+++ routes/web.php
@@ -15,37 +15,48 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return redirect('/login');
 });
 
-Route::get('/login', function() {
-    return view('auth.login');
+Route::get('/home', function () {
+    return redirect('/movies');
 });
 
-Route::get('/register', function(){
-    return view('auth.register');
+Route::group(['middleware' => ['guest']], function(){
+    Route::get('/login', function () {
+        return view('auth.login');
+    });
+
+    Route::post('/login', [UserController::class, 'login']);
+
+    Route::get('/register', function () {
+        return view('auth.register');
+    });
+
+    Route::post('/register', [UserController::class, 'register']);
 });
 
-Route::post('/logout', [UserController::class, 'logoutUser']);
+Route::get('/logout', [UserController::class, 'logoutUser']);
 Route::get('/profile', [UserController::class, 'editProfile']);
 Route::post('/profile/update', [UserController::class, 'update']);
 
-
 // Movie
-Route::group(['prefix' => 'movies'], function (){
+Route::group(['prefix' => 'movies'], function () {
     Route::get('/', [MovieController::class, 'index']);
-    Route::get('/detail', [MovieController::class, 'movieDetail']);
-    Route::get('/create', [MovieController::class, 'create']);
-    Route::post('/insert', [MovieController::class, 'store']);
-    Route::get('/edit/{id}', [MovieController::class, 'edit']);
-    Route::get('/editMovie/{id}', [MovieController::class, 'update']);
-    Route::post('/deleteMovie/{id}', [MovieController::class, 'delete']);
+    Route::get('/detail/{id}', [MovieController::class, 'movieDetail']);
+
+    Route::group(['middleware' => 'user-role'], function () {
+        Route::get('/create', [MovieController::class, 'create']);
+        Route::post('/insert', [MovieController::class, 'store']);
+        Route::get('/edit/{id}', [MovieController::class, 'edit']);
+        Route::get('/editMovie/{id}', [MovieController::class, 'update']);
+        Route::post('/deleteMovie/{id}', [MovieController::class, 'delete']);
+    });
 });
 
 // Actor
-Route::group(['prefix' => 'actors'], function(){
+Route::group(['prefix' => 'actors'], function () {
     Route::get('/', [ActorController::class, 'index']);
     Route::get('/create', [Actorcontroller::class, 'create']);
     Route::post('/insert', [ActorController::class, 'store']);
